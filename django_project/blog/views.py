ff16d3c5cc6d16d8f092d951e1fa498af1ac1b1b
luor3 <36827601+luor3@users.noreply.github.com>
2019-06-19 01:47:54 -0500

    Add files via upload

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,35 +1,13 @@
 from django.shortcuts import render
-
-posts = [
-        {'title':"Don't forget, a person's greatest emotional need is to feel appreciated.",
-        'date_posted': 'August 27, 2018',
-        'author': 'user#2018'
-        }
-    ,
-    
-       {'title':"Our core emotional need is to feel valued.",
-        'date_posted': 'August 27, 2017',
-        'author':'user#2017'
-       }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts 
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
-
-def login(request):
-    return render(request, 'blog/login.html', {'title': 'Login'})
-
-def register(request):
-    return render(request, 'blog/register.html', {'title': 'Register'})
-
-def contact(request):
-    return render(request, 'blog/contact.html', {'title': 'Contact'})
-
